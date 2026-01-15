e4e71236e782787857ca42a535163eae8726e063
Minh Phuong Dang <minhphuonglcby@gmail.com>
2026-01-15 10:27:11 +0700

    Update (signup.dart): handle validation phone field with formz & handle state management with cubit

--- lib/ui/signup/signup.dart
+++ lib/ui/signup/signup.dart
@@ -1,5 +1,8 @@
+import 'cubit/signup_state.dart';
+import 'cubit/signup_cubit.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../utils/bottom_action_section.dart';
 
@@ -11,59 +14,55 @@ class SignupWidget extends StatefulWidget {
 
 class _SignupWidgetState extends State<SignupWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String? _phoneStatus;
-  bool _isValidPhone = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(leading: Icon(Icons.arrow_back_ios_rounded)),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(24, 0, 24, 16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                _HeaderSection(),
-                const SizedBox(height: 21),
-                _PhoneInputSection(
-                  isValid: _isValidPhone,
-                  onValidate: (valid) {
-                    setState(() => _isValidPhone = valid);
-                  },
-                  onStatusChange: (status) {
-                    setState(() => _phoneStatus = status);
-                  },
-                ),
-                if (_isValidPhone)
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8),
-                      child: Text(
-                        _phoneStatus!,
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          color: Color(0xFF92C73D),
-                        ),
+    return BlocProvider(
+      create: (context) => SignupCubit(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(leading: Icon(Icons.arrow_back_ios_rounded)),
+        body: BlocBuilder<SignupCubit, SignupState>(
+          builder: (context, state) {
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(24, 0, 24, 16),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      _HeaderSection(),
+                      const SizedBox(height: 21),
+                      _PhoneInputSection(
+                        isValid: state.phone.isValid,
                       ),
-                    ),
+                      if (state.phone.isValid)
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Text(
+                              'Số điện thoại khả dụng',
+                              style: Theme.of(context).textTheme.bodySmall
+                                  ?.copyWith(color: Color(0xFF92C73D)),
+                            ),
+                          ),
+                        ),
+                      Expanded(child: Container()),
+                      BottomActionSection(
+                        button: 'Tiếp tục',
+                        text: 'đăng ký',
+                        textButton: 'Đăng nhập',
+                        onContinue: () {},
+                        onChangedSign: () {
+                          context.go('/');
+                        },
+                      ),
+                    ],
                   ),
-                Expanded(child: Container()),
-                BottomActionSection(
-                  button: 'Tiếp tục',
-                  text: 'đăng ký',
-                  textButton: 'Đăng nhập',
-                  onContinue: () {
-
-                  },
-                  onChangedSign: () {
-                    context.go('/');
-                  },
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -91,25 +90,18 @@ class _HeaderSection extends StatelessWidget {
 }
 
 class _PhoneInputSection extends StatelessWidget {
-  final ValueChanged<bool> onValidate;
-  final ValueChanged<String?> onStatusChange;
   final bool isValid;
 
   const _PhoneInputSection({
-    required this.onValidate,
-    required this.onStatusChange,
     required this.isValid,
   });
 
   @override
   Widget build(BuildContext context) {
-    
     return Container(
       height: 62,
       decoration: BoxDecoration(
-        border: Border.all(
-          color: isValid ? Color(0xFF92C73D) : Colors.grey,
-        ),
+        border: Border.all(color: isValid ? Color(0xFF92C73D) : Colors.grey),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Row(
@@ -128,7 +120,7 @@ class _PhoneInputSection extends StatelessWidget {
           ),
           const VerticalDivider(width: 1, indent: 16, endIndent: 16),
           const SizedBox(width: 4),
-          
+
           Expanded(
             child: TextFormField(
               keyboardType: TextInputType.phone,
@@ -145,18 +137,7 @@ class _PhoneInputSection extends StatelessWidget {
                     : null,
               ),
               onChanged: (value) {
-                final phoneRegExp = RegExp(r'^[0-9]+$');
-
-                if (value.isNotEmpty &&
-                    phoneRegExp.hasMatch(value) &&
-                    value.length >= 10 &&
-                    value.length <= 11) {
-                  onValidate(true);
-                  onStatusChange('Số điện thoại khả dụng');
-                } else {
-                  onValidate(false);
-                  onStatusChange(null);
-                }
+                context.read<SignupCubit>().phoneChanged(value);
               },
             ),
           ),
